ce82aa4a547f4b53ebc62c2ada93a62acddda921
migayalach <ayalachavezmiguel@gmail.com>
2024-01-15 20:18:40 -0400

    solo se verifico el funcionamiento del backend

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -7,11 +7,11 @@ const {
 } = require("../Handlers/recipes");
 const recipesRouter = Router();
 
-// CREAR RECIPE
-recipesRouter.post("/", validateCreateRecipe, createRecipeHandler);
-
 //TRAE TODAS LAS RECETAS SI NO SE MANDA UN QUERY, PERO SI SE MANDA TRAE LO QUE SE BUSCA
 recipesRouter.get("/", getRecipesHandler);
+
+// CREAR RECIPE
+recipesRouter.post("/", validateCreateRecipe, createRecipeHandler);
 
 // BUSCA POR ID RECIBIDO POR PARAMS
 recipesRouter.get("/:idRecipe", getRecipeHandler);
