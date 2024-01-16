224c90dc44949663ec0c86c463fa8db7b5ae768e
migayalach <ayalachavezmiguel@gmail.com>
2024-01-16 00:11:32 -0400

    index con todas los componentes principales

--- client/src/View/index.js
+++ client/src/View/index.js
@@ -4,5 +4,6 @@ import Error from "../View/Error/Error";
 import Form from "../View/Form/Form";
 import Home from "../View/Home/Home";
 import Favotire from "../View/Favorite/Favotire";
+import Login from "../View/Login/Login";
 
-export { About, Detail, Error, Form, Home, Favotire };
+export { About, Detail, Error, Form, Home, Favotire, Login };
