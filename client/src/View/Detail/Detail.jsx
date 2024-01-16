4ce2492a5b9293bf1ff556eb314470a21367e6e0
migayalach <ayalachavezmiguel@gmail.com>
2024-01-16 00:14:15 -0400

    detail

--- client/src/View/Detail/Detail.jsx
+++ client/src/View/Detail/Detail.jsx
@@ -1,19 +1,30 @@
 // HOOK'S
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 // COMPONENTS
+import Cards from "../../Components/Cards";
 
 // REDUX
+import { getRecipeId } from "../../Redux/actions";
 
 // CSS
 
 // JAVASCRIP
 
-
 function Detail() {
+  const dispatch = useDispatch();
+  const { idRecipe } = useParams();
+  const selectRecipe = useSelector((state) => state.aux);
+
+  useEffect(() => {
+    dispatch(getRecipeId(idRecipe));
+  }, [idRecipe]);
+
   return (
     <>
-      <div>Detail</div>
+      <Cards recipes={selectRecipe} />
     </>
   );
 }
