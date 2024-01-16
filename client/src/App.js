6ff73c10eee4e50d754f90c54202934f437d31ec
migayalach <ayalachavezmiguel@gmail.com>
2024-01-16 00:09:43 -0400

    app con rutas listo

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { Switch, Route, useLocation } from "react-router-dom";
 
 // COMPONENTS
-import { About, Detail, Error, Form, Home, Favotire } from "./View";
+import { About, Detail, Error, Form, Home, Favotire, Login } from "./View";
 import NavBar from "./Components/NavBar/NavBar";
 
 // REDUX
@@ -14,10 +14,16 @@ import "./StyleSheets/App.css";
 // JAVASCRIP
 
 const App = () => {
+  const location = useLocation();
+  const [access, setAccess] = useState(false);
+
+  useEffect(() => {}, []);
+
   return (
     <div className="App">
-      <NavBar />
+      {location.pathname !== "/" && <NavBar />}
       <Switch>
+        <Route exact path="/" component={Login} />
         <Route path="/home" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/form" component={Form} />
