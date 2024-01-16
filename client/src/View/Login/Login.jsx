818fe6e0fc480fe61512dab49a2573b270d36454
migayalach <ayalachavezmiguel@gmail.com>
2024-01-16 00:13:27 -0400

    login

---
+++ client/src/View/Login/Login.jsx
@@ -0,0 +1,22 @@
+// HOOK'S
+import React, { useState, useEffect } from "react";
+
+// COMPONENTS
+import Button from "../../Components/Button";
+
+// REDUX
+
+// CSS
+
+// JAVASCRIP
+
+function Login() {
+  return (
+    <>
+      <div>Login</div>
+      <Button />
+    </>
+  );
+}
+
+export default Login;
