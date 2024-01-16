6e5b24cdfbb4515200d9b6b8d7bf40e56495cf10
migayalach <ayalachavezmiguel@gmail.com>
2024-01-16 00:13:53 -0400

    home

--- client/src/View/Home/Home.jsx
+++ client/src/View/Home/Home.jsx
@@ -1,19 +1,30 @@
 // HOOK'S
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 // COMPONENTS
+import Cards from "../../Components/Cards";
+import Filter from "../../Components/Filter/Filter";
 
 // REDUX
+import { getRecipeAll } from "../../Redux/actions";
 
 // CSS
 
 // JAVASCRIP
 
-
 function Home() {
+  const dispatch = useDispatch();
+  const selectRecipe = useSelector((state) => state.recipes);
+
+  useEffect(() => {
+    dispatch(getRecipeAll());
+  }, []);
+
   return (
     <>
-      <div>Home</div>
+      <Filter />
+      <Cards recipes={selectRecipe} />
     </>
   );
 }
