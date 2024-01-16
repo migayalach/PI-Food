044d01af73f0924ab92a5d00f1ed384aab9d977e
migayalach <ayalachavezmiguel@gmail.com>
2024-01-16 00:20:03 -0400

    button

---
+++ client/src/Components/Button.jsx
@@ -0,0 +1,20 @@
+// HOOK'S
+import React from "react";
+
+// COMPONENTS
+
+// REDUX
+
+// CSS
+
+// JAVASCRIP
+
+function Button() {
+  const handlerLogin = () => {
+    alert("hola");
+  };
+
+  return <button onClick={handlerLogin}>Button</button>;
+}
+
+export default Button;
