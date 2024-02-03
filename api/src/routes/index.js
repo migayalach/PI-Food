96ba7ee586139b206bd303a8d49f8dbd15bb8a35
migayalach <ayalachavezmiguel@gmail.com>
2024-02-02 23:21:07 -0400

    se agrego la ruta updateData

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,13 @@
-const { Router } = require('express');
+const { Router } = require("express");
 
 const dietsRouter = require("./diets");
 const recipesRouter = require("./recipes");
+const updateData = require("./updateData");
+
 const router = Router();
 
-
-router.use("/diets", dietsRouter)//ok
+router.use("/diets", dietsRouter);
 router.use("/recipes", recipesRouter);
+router.use("/updateData", updateData);
 
 module.exports = router;
