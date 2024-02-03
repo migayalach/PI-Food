f73b27f10a933a4ec65506c7166439acd081aa3c
migayalach <ayalachavezmiguel@gmail.com>
2024-02-02 23:22:26 -0400

    se limpio la ruta recipes

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -7,14 +7,8 @@ const {
 } = require("../Handlers/recipes");
 const recipesRouter = Router();
 
-//TRAE TODAS LAS RECETAS SI NO SE MANDA UN QUERY, PERO SI SE MANDA TRAE LO QUE SE BUSCA
 recipesRouter.get("/", getRecipesHandler);
-
-// CREAR RECIPE
 recipesRouter.post("/", validateCreateRecipe, createRecipeHandler);
-
-// BUSCA POR ID RECIBIDO POR PARAMS
 recipesRouter.get("/:idRecipe", getRecipeHandler);
 
-// recipesRouter.post("/", createRecipe);
 module.exports = recipesRouter;
