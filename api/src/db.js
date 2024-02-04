304ed8ad7a9c9506bf6541bf12a211bb8c2bfac9
migayalach <ayalachavezmiguel@gmail.com>
2024-02-03 22:54:46 -0400

    se agrego la nueva tabla intermedia creada DietsRecipe

--- api/src/db.js
+++ api/src/db.js
@@ -37,20 +37,20 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Recipe, Diets } = sequelize.models;
+const { Recipe, Diets, DietsRecipe } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 Recipe.belongsToMany(
   Diets,
-  { through: "RecipeDiets" },
+  { through: DietsRecipe, foreignKey: "idRecipe" },
   {
     timestamps: false,
   }
 );
 Diets.belongsToMany(
   Recipe,
-  { through: "RecipeDiets" },
+  { through: DietsRecipe, foreignKey: "idDiet" },
   {
     timestamps: false,
   }
