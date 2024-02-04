dc2a4639a82055b4e5abec5efe5fe9010d7bb350
migayalach <ayalachavezmiguel@gmail.com>
2024-02-03 23:00:36 -0400

    se agrego el handler update

--- api/src/Handlers/recipes.js
+++ api/src/Handlers/recipes.js
@@ -3,21 +3,23 @@ const {
   buscarRecipe,
   mostrarAllRecipe,
   getRecipeData,
+  putRecipeController,
 } = require("../Controllers/recipesControllers");
 const SUCCESS = 200,
   ERROR = 400;
 
 //CREAR RECIPE
 const createRecipeHandler = async (request, response) => {
-  const { nombre, imagen, resumen, nivel, pasoApaso, dieta } = request.body;
+  const { nameRecipe, imageRecipe, summary, healthScore, created, diets } =
+    request.body;
   try {
     const newRecipe = await createRecipes(
-      nombre,
-      imagen,
-      resumen,
-      nivel,
-      pasoApaso,
-      dieta
+      nameRecipe,
+      imageRecipe,
+      summary,
+      healthScore,
+      created,
+      diets
     );
     response.status(SUCCESS).json({ success: newRecipe });
   } catch (error) {
@@ -39,9 +41,8 @@ const getRecipesHandler = async (request, response) => {
 // MUESTRA DATOS POR ID -> PARAMS
 const getRecipeHandler = async (request, response) => {
   const { idRecipe } = request.params;
-  const typeData = isNaN(idRecipe) ? "bdd" : "api";
   try {
-    const reciper = await getRecipeData(typeData, idRecipe);
+    const reciper = await getRecipeData(idRecipe);
     console.log(reciper);
     response.status(SUCCESS).json(reciper);
   } catch (error) {
@@ -49,8 +50,35 @@ const getRecipeHandler = async (request, response) => {
   }
 };
 
+const updateRecipeHandler = async (request, response) => {
+  const {
+    idRecipe,
+    nameRecipe,
+    imageRecipe,
+    summary,
+    healthScore,
+    created,
+    diets,
+  } = request.body;
+  try {
+    const data = await putRecipeController(
+      idRecipe,
+      nameRecipe,
+      imageRecipe,
+      summary,
+      healthScore,
+      created,
+      diets
+    );
+    response.status(200).json(data);
+  } catch (error) {
+    response.status(400).json({ message: error.message });
+  }
+};
+
 module.exports = {
   createRecipeHandler,
   getRecipesHandler,
   getRecipeHandler,
+  updateRecipeHandler,
 };
