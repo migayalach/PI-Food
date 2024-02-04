56090a3f6e9689d22db230603cce44a7016ee31c
migayalach <ayalachavezmiguel@gmail.com>
2024-02-03 22:57:18 -0400

    se agrego la opcion de editar

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -1,14 +1,19 @@
 const { Router } = require("express");
-const { validateRecipeName, validateCreateRecipe } = require("../Middlewares/recipeMiddleware");
+const {
+  validateRecipeName,
+  validateCreateRecipe,
+} = require("../Middlewares/recipeMiddleware");
 const {
   createRecipeHandler,
   getRecipesHandler,
   getRecipeHandler,
+  updateRecipeHandler,
 } = require("../Handlers/recipes");
 const recipesRouter = Router();
 
 recipesRouter.get("/", getRecipesHandler);
-recipesRouter.post("/", validateCreateRecipe, createRecipeHandler);
 recipesRouter.get("/:idRecipe", getRecipeHandler);
+recipesRouter.post("/", createRecipeHandler);
+recipesRouter.put("/", updateRecipeHandler);
 
 module.exports = recipesRouter;
