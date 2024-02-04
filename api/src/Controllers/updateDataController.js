9bdb3b4075a820197e15c1692953f4e6198ee86a
migayalach <ayalachavezmiguel@gmail.com>
2024-02-03 23:02:26 -0400

    upadte informacion listo, para traer las recetas y dietas de la base de datos local, donde se encuentran los datos de la api

--- api/src/Controllers/updateDataController.js
+++ api/src/Controllers/updateDataController.js
@@ -1,9 +1,14 @@
-const { Diets } = require("../db");
+const { Diets, Recipe } = require("../db");
 const localBdd = require("../Utils/dataBaseLocal");
 const { clearDiets, reviewDiets } = require("../helpers/clearDataBdd");
-const { existDiets } = require("./helperController");
+const {
+  existDiets,
+  dietsPrommise,
+  addRecipeDiets,
+} = require("./helperController");
 
 const updateDataBdd = async () => {
+  // PRIMERO AGREGAR DIETAS
   const dataDietsBdd = (await existDiets()).map(({ nameDiet }) => nameDiet);
   const dataDiets = (await localBdd.map(({ diets }) => diets)).flat();
   const dietsResult = clearDiets(dataDiets);
@@ -13,6 +18,37 @@ const updateDataBdd = async () => {
   }
   await Promise.all(
     dietsData.map(async (nameDiet) => await Diets.create({ nameDiet }))
+  );
+
+  if (localBdd.length === (await Recipe.count())) {
+    throw Error`No hay recetas nuevas para poder agregar a la base de datos`;
+  }
+
+  // SEGUNDO AGREGAR RECETAS
+  const dataRecipe = localBdd.map(
+    async ({ healthScore, title, image, summary, diets }) => ({
+      healthScore,
+      title,
+      image,
+      summary,
+      diets: await dietsPrommise(diets),
+    })
+  );
+  const resPromisseRecipe = await Promise.all(dataRecipe);
+  // AGREGAR A LA TABLA RECETAS
+  await Promise.all(
+    resPromisseRecipe.map(
+      async ({ healthScore, title, image, summary, diets }) => {
+        const { idRecipe } = await Recipe.create({
+          nameRecipe: title,
+          imageRecipe: image,
+          summary,
+          healthScore,
+        });
+        // AGREGAR A LA TABLA INTERMEDIA
+        return await addRecipeDiets(idRecipe, diets);
+      }
+    )
   );
   return `Datos introducidos con exito`;
 };
