f75c94a32faa105149513a3038d3c9e93097b0ba
migayalach <ayalachavezmiguel@gmail.com>
2024-02-03 23:04:02 -0400

    este archivo tiene las funciones que se desea reutilizar

--- api/src/Controllers/helperController.js
+++ api/src/Controllers/helperController.js
@@ -1,9 +1,30 @@
-const { Recipe, Diets } = require("../db");
+const { Recipe, Diets, DietsRecipe } = require("../db");
+const { Op } = require("sequelize");
 
 async function existDiets() {
   return await Diets.findAll();
 }
 
+async function dietsPrommise(diets) {
+  const diet = await Promise.all(
+    diets.map((nameDiet) =>
+      Diets.findOne({ where: { nameDiet: { [Op.like]: `${nameDiet}` } } })
+    )
+  );
+  return diet.map(({ idDiet }) => idDiet);
+}
+
+async function addRecipeDiets(idRecipe, diets) {
+  await Promise.all(
+    diets.map(async (idDiet) => {
+      return DietsRecipe.create({ idDiet, idRecipe });
+    })
+  );
+  return `Receta creada con exito`;
+}
+
 module.exports = {
   existDiets,
+  dietsPrommise,
+  addRecipeDiets,
 };
