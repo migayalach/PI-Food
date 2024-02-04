ffa36e0a8c227fe7afe83c3bbc05f800381ee6fe
migayalach <ayalachavezmiguel@gmail.com>
2024-02-03 23:04:24 -0400

    controlador diets listo

--- api/src/Controllers/dietsControllers.js
+++ api/src/Controllers/dietsControllers.js
@@ -1,23 +1,7 @@
-const axios = require("axios");
-const { Diets, Recipe } = require("../db");
-const { API_KEY } = process.env;
-const URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=100&addRecipeInformation=true`;
-const results = require("../Utils/dataBaseLocal");
-const { clearArray } = require("../Utils/dietsUtils");
-
-const addBdd = async (arr) => await Diets.bulkCreate(arr);
+const { Diets } = require("../db");
 
 const recivedDiet = async () => {
-  // const data = await Diets.count();
-  // if (data) return Diets.findAll();
-  // return await loadingApi();
-  return results
-};
-
-const loadingApi = async () => {
-  const apiDiets = (await axios.get(`${URL}`)).data.results;
-  const apiResult = clearArray(apiDiets);
-  return await addBdd(apiResult);
+  return await Diets.findAll();
 };
 
 module.exports = {
