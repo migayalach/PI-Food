f4e4eb084b601cae6a45c3826270d4afab9ff60d
migayalach <ayalachavezmiguel@gmail.com>
2024-02-08 19:42:37 -0400

    middlewares para recipes

--- api/src/Middlewares/recipeMiddleware.js
+++ api/src/Middlewares/recipeMiddleware.js
@@ -1,23 +1,85 @@
 const ERROR = 400;
 
 const validateRecipeName = (request, response, next) => {
-  const {name} = request.query;
-  if(!name) return response.status(ERROR).send("Introduce el nombre de la receta que quieras buscar");
+  const { name } = request.query;
+  if (!name)
+    return response
+      .status(ERROR)
+      .send("Introduce el nombre de la receta que quieras buscar");
+  next();
+};
+
+const validateRecipeId = (request, response, next) => {
+  const { idRecipe } = request.body;
+  if (isNaN(idRecipe))
+    return response
+      .status(ERROR)
+      .json({ error: `Lo siento el identificador enviado no es valido` });
+  const number = parseInt(idRecipe);
+  if (number < 1)
+    return response
+      .status(ERROR)
+      .json({ error: "Por favor introduzca un identificador mayor a 0" });
+  next();
+};
+
+const validateRecipeIdParams = (request, response, next) => {
+  const { idRecipe } = request.params;
+  if (isNaN(idRecipe))
+    return response
+      .status(ERROR)
+      .json({ error: `Lo siento el identificador enviado no es valido` });
+  const number = parseInt(idRecipe);
+  if (number < 1)
+    return response
+      .status(ERROR)
+      .json({ error: "Por favor introduzca un identificador mayor a 0" });
   next();
 };
 
 const validateCreateRecipe = (request, response, next) => {
-  const { nombre, imagen, resumen, nivel, pasoApaso, dieta } = request.body;
-  if(!nombre) return response.status(ERROR).send("Falta nombre");
-  if(!imagen) return response.status(ERROR).send("Falta un URL de la imagen de la receta");
-  if(!resumen) return response.status(ERROR).send("Por favor agrega un resumen para esta receta");
-  if(!nivel) return response.status(ERROR).send("Agrega un nivel de dificultad para la preparacion de la receta");
-  if(!pasoApaso) return response.status(ERROR).send("Por favor agrega los pasos que seguiste para esta receta");
-  if(dieta.length<1) return response.status(ERROR).send("Introduce por lo menos una dieta");
+  const { nameRecipe, imageRecipe, summary, healthScore, created, diets } =
+    request.body;
+
+  if (!nameRecipe)
+    return response.status(ERROR).json({ error: "Falta nombre" });
+
+  if (!imageRecipe)
+    return response
+      .status(ERROR)
+      .json({ error: "Falta una URL para la imagen" });
+
+  if (!summary)
+    return response
+      .status(ERROR)
+      .json({ error: "Por favor agrega un resumen para esta receta" });
+
+  if (summary.length < 100) {
+    return response
+      .status(ERROR)
+      .json({ error: "Por favor agrega un resumen mas largo" });
+  }
+
+  if (!parseFloat(healthScore))
+    return response.status(ERROR).json({
+      error: "Por favor introduzca una calificacion",
+    });
+
+  if (created !== true)
+    return response
+      .status(ERROR)
+      .json({ error: "Usted no tiene permiso para alterar este campo" });
+
+  if (!diets.length)
+    return response
+      .status(ERROR)
+      .json({ error: "Introduce por lo menos una dieta" });
   next();
-}
+};
 
 module.exports = {
   validateRecipeName,
-  validateCreateRecipe
-}
+  validateCreateRecipe,
+  validateRecipeId,
+  validateRecipeIdParams,
+};
