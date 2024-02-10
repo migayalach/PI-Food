6899dac7570d799ee770d4dccd1bc8799c2a3f12
migayalach <ayalachavezmiguel@gmail.com>
2024-02-10 19:18:52 -0400

    boton que se encarga de despachar la accion

--- client/src/Components/Button.jsx
+++ client/src/Components/Button.jsx
@@ -1,20 +1,24 @@
 // HOOK'S
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 // COMPONENTS
 
 // REDUX
+import { getPagination } from "../Redux/actions";
 
 // CSS
 
 // JAVASCRIP
 
-function Button({ pageNumber }) {
+function Button({ number, action }) {
+  const dispatch = useDispatch();
+
   const handlerLogin = () => {
-    alert("hola");
+    dispatch(getPagination(number));
   };
 
-  return <button onClick={handlerLogin}>{pageNumber}</button>;
+  return <button onClick={handlerLogin}>{number}</button>;
 }
 
 export default Button;
