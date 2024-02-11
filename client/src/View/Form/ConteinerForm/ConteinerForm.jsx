b925abbaad72b5e8c6265b56869aa1cbef2bf388
migayalach <ayalachavezmiguel@gmail.com>
2024-02-11 10:46:26 -0400

    contenedor del formulario de creacion

---
+++ client/src/View/Form/ConteinerForm/ConteinerForm.jsx
@@ -0,0 +1,22 @@
+// HOOK'S
+import React from "react";
+
+// COMPONENTS
+import CreateRecipe from "../CreateRecipe/CreateRecipe";
+
+// REDUX
+
+// CSS
+
+// JAVASCRIP
+
+function ConteinerForm() {
+  return (
+    <>
+      <div>Create a new recipe</div>
+      <CreateRecipe />
+    </>
+  );
+}
+
+export default ConteinerForm;
