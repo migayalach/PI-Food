cd7055d02d9d5b9a5f8f05a5a15803a6fda25431
migayalach <ayalachavezmiguel@gmail.com>
2024-02-11 10:48:28 -0400

    exportacion del contenedor de form

--- client/src/View/index.js
+++ client/src/View/index.js
@@ -1,9 +1,9 @@
 import About from "../View/About/About";
 import Detail from "../View/Detail/Detail";
 import Error from "../View/Error/Error";
-import Form from "../View/Form/Form";
+import ConteinerForm from "../View/Form/ConteinerForm/ConteinerForm";
 import Home from "../View/Home/Home";
 import Favotire from "../View/Favorite/Favotire";
 import Login from "../View/Login/Login";
 
-export { About, Detail, Error, Form, Home, Favotire, Login };
+export { About, Detail, Error, ConteinerForm, Home, Favotire, Login };
