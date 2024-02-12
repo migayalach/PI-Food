a94f7894d0662551456b50a9edd804d48b85bea5
migayalach <ayalachavezmiguel@gmail.com>
2024-02-11 21:48:15 -0400

    formulario para crear recetas

--- client/src/View/Form/CreateRecipe/CreateRecipe.jsx
+++ client/src/View/Form/CreateRecipe/CreateRecipe.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 // COMPONENTS
+import ConteinerForm from "../../../Components/FormDiets/ConteinerForm/ConteinerForm";
 
 // REDUX
 import { getAllDiets } from "../../../Redux/actions";
@@ -14,6 +15,17 @@ import { getAllDiets } from "../../../Redux/actions";
 function CreateRecipe() {
   const dispatch = useDispatch();
   const selectDiets = useSelector((state) => state.diets);
+  const [recipe, setRecipe] = useState({
+    nameRecipe: "",
+    image: "",
+    summary: "",
+    healthScore: 0,
+    diets: [],
+  });
+
+  const handleFormRecipe = (event) => {
+    setRecipe({ ...recipe, [event.target.name]: event.target.value });
+  };
 
   useEffect(() => {
     dispatch(getAllDiets());
@@ -23,31 +35,43 @@ function CreateRecipe() {
     <form>
       <div>
         <label htmlFor="name-recipe">Name recipe</label>
-        <input type="text" />
+        <input
+          type="text"
+          // value={recipe.nameRecipe}
+          name="nameRecipe"
+          onChange={handleFormRecipe}
+        />
       </div>
       <div>
         <label htmlFor="image-recipe">Image recipe</label>
-        <input type="text" />
+        <input
+          type="text"
+          // value={recipe.image}
+          name="image"
+          onChange={handleFormRecipe}
+        />
       </div>
       <div>
         <label htmlFor="summary">Summary</label>
-        <textarea name="summary" rows="4" cols="50"></textarea>
+        <textarea
+          type="text"
+          name="summary"
+          // value={recipe.summary}
+          onChange={handleFormRecipe}
+          rows="4"
+          cols="50"
+        ></textarea>
       </div>
       <div>
         <label htmlFor="health-score">HealthScore</label>
-        <input type="number" />
+        <input
+          type="number"
+          name="healthScore"
+          // value={recipe.healthScore}
+          onChange={handleFormRecipe}
+        />
       </div>
-      <div>
-        <label htmlFor="diets">Diets</label>
-        <select name="" value="" onChange="">
-          <option value=""></option>
-          {selectDiets.map(({ idDiet, nameDiet }, index) => (
-            <option key={index} value={idDiet}>
-              {nameDiet}
-            </option>
-          ))}
-        </select>
-      </div>
+      <ConteinerForm />
     </form>
   );
 }
