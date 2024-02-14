93a3d7d76c1728218ecee2de0e93358760ae1810
migayalach <ayalachavezmiguel@gmail.com>
2024-02-14 13:16:11 -0400

    mensajes de error

---
+++ client/src/Components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,18 @@
+// HOOK'S
+import React from "react";
+
+// COMPONENTS
+
+// REDUX
+
+// CSS
+
+// JAVASCRIP
+
+function ErrorMessage() {
+  return (
+    <div>ErrorMessage</div>
+  )
+}
+
+export default ErrorMessage
