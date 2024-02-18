78eb478cfd6f2f73c1858f6671de62279e05f87e
migayalach <ayalachavezmiguel@gmail.com>
2024-02-18 17:25:10 -0400

    informacion

--- client/src/View/About/About.jsx
+++ client/src/View/About/About.jsx
@@ -6,15 +6,34 @@ import React from "react";
 // REDUX
 
 // CSS
+import "./about.css";
 
 // JAVASCRIP
 
-
 function About() {
   return (
-    <>
-      <div>About</div>
-    </>
+    <div className="about-container">
+      <div className="about-description">
+        <h2>About Me</h2>
+        <p>
+          Hi there! 👋 I'm Miguel, a full-stack developer currently living in La
+          Paz, Bolivia 🇧🇴.
+        </p>
+        <p>🔭 I am currently looking for a job.</p>
+        <p>
+          📂 Main tech skills: JavaScript | React | Next.js | Redux | Java |
+          Node.js | Express | MySQL | Prisma | PostgreSQL | Testing.
+        </p>
+        <p>
+          📫 How to reach me:{" "}
+          <a href="mailto:ayalachavezmiguel@gmail.com">
+            ayalachavezmiguel@gmail.com
+          </a>
+          .
+        </p>
+        <p>Thanks for visiting my profile! 😄</p>
+      </div>
+    </div>
   );
 }
 
