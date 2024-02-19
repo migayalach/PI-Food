42ef9fc907fca1a456236cfcc9c3a091365e49be
migayalach <ayalachavezmiguel@gmail.com>
2024-02-18 22:56:55 -0400

    se creo la variable favorite

--- client/src/Redux/actionsType.js
+++ client/src/Redux/actionsType.js
@@ -2,4 +2,5 @@ export const GET_RECIPES = "GET_RECIPES";
 export const SEARCH_REC_ID = "SEARCH_REC_ID";
 export const GET_DIETS = "GET_DIETS";
 export const CREATE_RECIPE = "CREATE_RECIPE";
+export const FAVORITE = "FAVORITE";
 export const ERROR = "ERROR";
