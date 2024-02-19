54db222ff20cf3c0224d82c1b36a589cd527ed64
migayalach <ayalachavezmiguel@gmail.com>
2024-02-18 20:28:44 -0400

    login

--- client/src/View/Login/Login.jsx
+++ client/src/View/Login/Login.jsx
@@ -1,21 +1,37 @@
 // HOOK'S
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 // COMPONENTS
 import Button from "../../Components/Button";
+import { QqOutlined } from "@ant-design/icons";
+
+// LIBRARY
 
 // REDUX
 
 // CSS
+import "./login.css";
+
+// IMAGE
+const image = require("../../Image/fondo-menu.jpg");
 
 // JAVASCRIP
 
 function Login() {
+  const history = useHistory();
+  const handleLogin = () => {
+    history.push("/home");
+  };
+
   return (
-    <>
-      <div>Login</div>
-      {/* <Button /> */}
-    </>
+    <div className="conteiner-login">
+      <img src={image} alt="imagen de fondo" className="image" />
+      <button className="button" onClick={handleLogin}>
+        Login
+        <QqOutlined className="logo" />
+      </button>
+    </div>
   );
 }
 
