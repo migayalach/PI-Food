8ffa5b6063ec6640a4bf606f85477a909d43960a
migayalach <ayalachavezmiguel@gmail.com>
2024-02-21 22:08:04 -0400

    se agrego la variable global filter

--- client/src/Redux/actionsType.js
+++ client/src/Redux/actionsType.js
@@ -3,4 +3,5 @@ export const SEARCH_REC_ID = "SEARCH_REC_ID";
 export const GET_DIETS = "GET_DIETS";
 export const CREATE_RECIPE = "CREATE_RECIPE";
 export const FAVORITE = "FAVORITE";
+export const FILTER = "FILTER";
 export const ERROR = "ERROR";
