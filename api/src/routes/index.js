b6cf59c74aa4ad080d757bf3640ba61d422bc5a0
migayalach <ayalachavezmiguel@gmail.com>
2024-02-21 22:16:37 -0400

    se agrego la ruta filter

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 const dietsRouter = require("./diets");
 const recipesRouter = require("./recipes");
 const updateData = require("./updateData");
+const filterRouter = require("./filter");
 
 const router = Router();
 
 router.use("/diets", dietsRouter);
 router.use("/recipes", recipesRouter);
 router.use("/updateData", updateData);
+router.use("/filter", filterRouter);
 
 module.exports = router;
